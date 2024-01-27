43f560126de66517a3fbb02c777fd31a93a98258
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-01-27 11:30:13 +0200

    Mainly graphql and current user, avatar, button.
    New files graphql config, custom avatar, text. mutations, queries, schematypes, currency-number, get-data-colors, index for date, get name initials, get random color, helpers, utility index. and upades in vite config, tsconfig, current user, package  json.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
 });
